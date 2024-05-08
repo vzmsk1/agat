4a99b023e4295e52d86d3a0d6e529895e67d7f33
vyazemskaya <vyazemskayanna@gmail.com>
2024-05-08 14:41:53 +0300

    update homepage

--- src/js/lib/swiper.js
+++ src/js/lib/swiper.js
@@ -10,7 +10,7 @@ function initSlidersOnResize() {
         new Swiper('.activities__swiper', {
             modules: [Navigation, Pagination],
             speed: 800,
-            rewind: true,
+            loop: true,
             slidesPerView: mm.matches ? 1 : 3,
             spaceBetween: remToPx(4.2),
             navigation: {
@@ -19,6 +19,44 @@ function initSlidersOnResize() {
             },
             pagination: {
                 el: '.activities__pagination',
+                type: 'bullets',
+                clickable: true
+            }
+        });
+    }
+    if (document.querySelector('.products__swiper')) {
+        new Swiper('.products__swiper', {
+            modules: [Navigation, Pagination],
+            speed: 800,
+            loop: true,
+            slidesPerView: mm.matches ? 1 : 3,
+            spaceBetween: remToPx(4.2),
+            centeredSlides: true,
+            centeredSlidesBounds: true,
+            navigation: {
+                prevEl: '.products .i-btn_arr-prev',
+                nextEl: '.products .i-btn_arr-next'
+            },
+            pagination: {
+                el: '.products__pagination',
+                type: 'bullets',
+                clickable: true
+            }
+        });
+    }
+    if (document.querySelector('.news__swiper')) {
+        new Swiper('.news__swiper', {
+            modules: [Navigation, Pagination],
+            speed: 800,
+            loop: true,
+            slidesPerView: mm.matches ? 1 : 3,
+            spaceBetween: remToPx(4),
+            navigation: {
+                prevEl: '.news .i-btn_arr-prev',
+                nextEl: '.news .i-btn_arr-next'
+            },
+            pagination: {
+                el: '.news__pagination',
                 type: 'bullets',
                 clickable: true
             }
